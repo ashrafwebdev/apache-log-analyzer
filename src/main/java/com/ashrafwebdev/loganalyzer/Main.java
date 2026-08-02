855bb967591db254c9834dedfc562ef3eeaa291c
ashrafwebdev <ashrafbaarab@gmail.com>
2026-08-02 21:11:15 +0530

    Add Main to run the parser and print the extracted IP address

---
+++ src/main/java/com/ashrafwebdev/loganalyzer/Main.java
@@ -0,0 +1,12 @@
+package com.ashrafwebdev.loganalyzer;
+
+public class Main{
+
+    public static void main(String[] args){
+        String sampleLine = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326";
+        LogParser parser = new LogParser();
+        String ip = parser.extractIpAddress(sampleLine);
+        System.out.println(ip);
+
+    }
+}
