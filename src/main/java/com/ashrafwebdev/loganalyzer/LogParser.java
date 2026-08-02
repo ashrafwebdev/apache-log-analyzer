5ef8d1402ecceee6e319039e998e0666c79999aa
ashrafwebdev <ashrafbaarab@gmail.com>
2026-08-02 21:17:21 +0530

    Add textToNumber helper using Integer.parseInt

--- src/main/java/com/ashrafwebdev/loganalyzer/LogParser.java
+++ src/main/java/com/ashrafwebdev/loganalyzer/LogParser.java
@@ -5,4 +5,8 @@ public class LogParser{
         String[] parts = line.split(" ");
         return parts[0];
     }
+
+    public int textToNumber(String text){
+        return Integer.parseInt(text);
+    }
 }
