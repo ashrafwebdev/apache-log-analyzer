bfb5b6389885b1d88975ac4c8af28b8482f51343
ashrafwebdev <ashrafbaarab@gmail.com>
2026-08-02 21:46:17 +0530

    Add extractStatusAndBytes method

--- src/main/java/com/ashrafwebdev/loganalyzer/LogParser.java
+++ src/main/java/com/ashrafwebdev/loganalyzer/LogParser.java
@@ -9,4 +9,18 @@ public class LogParser{
     public int textToNumber(String text){
         return Integer.parseInt(text);
     }
+
+    public String[] extractRequestParts(String line){
+        String[] bySections = line.split("\"");
+        String request = bySections[1];
+        return request.split(" ");
+
+
+    }
+
+    public String[] extractStatusAndBytes(String line){
+        String[] bySections = line.split("\"");
+        String statusAndBytes = bySections[2].trim();
+        return statusAndBytes.split(" ");
+    }
 }
